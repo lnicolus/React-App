e6d838a717da951cb4a47e06be7299436dfe488e
Lnicolus <lnicolus86@gmail.com>
2022-02-10 22:48:21 -0300

    cartview completa para coderhouse, faltaba unicamente eliminar los detalles visibles de cartwidget cuando no habia ningun item seleccionado para compra

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useCartContext } from "../../Context/CartContext";
+
+
 export default function CartWidget() {
+  
+  const { cantidad, sumaTotal } = useCartContext()
   return (
     <div className="cart">
       <p className="text-light navText">Cart</p>
@@ -9,11 +14,17 @@ export default function CartWidget() {
           src="https://cdn1.iconfinder.com/data/icons/food-delivery-44/64/Grocery-bag-vegetable-shop-store-food-delivery-256.png"
           alt="buy"
         />
-     
+{ cantidad() !== 0 && cantidad() ? (
+     <>
       <ul className="shoppingStatusDisplay">
-        <li className="text-light navText">Products: </li>
-        <li className="text-light navText">Subtotal: </li>
+        <li className="text-light navText">{`Kilograms: ${cantidad()}`}</li>
+        <li className="text-light navText">{`Subtotal: ${sumaTotal()}`}</li>
       </ul>
+      </>)
+
+      : (<></>)
+    
+    }
     </div>
   );
 }
